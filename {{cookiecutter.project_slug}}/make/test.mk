46cb23b3544bb2e847ef1a0ef60d42680e34b2f1
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-04-16 04:51:19 +0200

    :bug: Fix spelling

--- {{cookiecutter.project_slug}}/make/test.mk
+++ {{cookiecutter.project_slug}}/make/test.mk
@@ -1,5 +1,5 @@
 ensure-test-dependencies:
 	command -v pytest 2>/dev/null || make install-tests
 
-est: ${VERSION} ensure-test-dependencies
+test: ${VERSION} ensure-test-dependencies
 	pytest tests/unit_tests
