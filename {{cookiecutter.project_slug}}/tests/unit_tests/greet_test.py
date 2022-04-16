f828aec051530a43ac6e44db9f59b4cdb6003d27
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-04-16 03:25:43 +0200

    :bug: Fix tests

--- {{cookiecutter.project_slug}}/tests/unit_tests/greet_test.py
+++ {{cookiecutter.project_slug}}/tests/unit_tests/greet_test.py
@@ -3,20 +3,20 @@ from unittest.mock import MagicMock
 import pytest
 from pytest_mock.plugin import MockerFixture
 
-from {{cookiecutter.project_slug}}.main import greet
+from {{cookiecutter.project_slug}}.main import welcome
 
 
 @pytest.fixture()
 def mocked_greet(mocker: MockerFixture) -> MagicMock:
     mocked = mocker.patch("typer.echo")
-    greet(name="Huba")
+    welcome(name="Joe")
     return mocked
 
 
 def test_greet_positive(mocked_greet: MagicMock) -> None:
-    mocked_greet.assert_called_with("Welcome Huba!")
+    mocked_greet.assert_called_with("Welcome Joe!")
 
 
 def test_greet_negative(mocked_greet: MagicMock) -> None:
     with pytest.raises(AssertionError):
-        mocked_greet.assert_called_with("Welcome Ruba!")
+        mocked_greet.assert_called_with("Welcome Karen!")
