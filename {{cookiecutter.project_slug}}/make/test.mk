a1700ce24aae676582ca4ac16b6282472dded8bf
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-04-16 04:50:42 +0200

    :construction_worker: Add Docker build yml workflow

--- {{cookiecutter.project_slug}}/make/test.mk
+++ {{cookiecutter.project_slug}}/make/test.mk
@@ -1,10 +1,5 @@
 ensure-test-dependencies:
 	command -v pytest 2>/dev/null || make install-tests
 
-test: unit-tests integration-tests
-
-unit-tests: ${VERSION} ensure-test-dependencies
+est: ${VERSION} ensure-test-dependencies
 	pytest tests/unit_tests
-
-integration-tests: ${VERSION} ensure-test-dependencies
-	pytest tests/integration_tests
